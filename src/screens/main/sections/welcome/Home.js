7800753e94d54b1d5f81b8a7a98bafb8a5d3494f
francis <francislow_6@hotmail.com>
2021-03-13 20:54:40 +0800

    increase user interaction area of button

--- src/screens/main/sections/welcome/Home.js
+++ src/screens/main/sections/welcome/Home.js
@@ -9,9 +9,11 @@ function Welcome() {
         <div id="home" className="welcome-page-wrapper">
             <p className="intro-header">Hello, I’m <span className="name-text">Francis</span></p>
             <p className="sub-intro">I love creating applications that add value to people's lives</p>
-            <div onClick={scrollToBioPage} className="read-bio-button">
-                <a href="#about">Read My Bio</a>
-            </div>
+            <a href="#about">
+                <div onClick={scrollToBioPage} className="read-bio-button">
+                    Read My Bio
+                </div>
+            </a>
         </div>
     )
 }
